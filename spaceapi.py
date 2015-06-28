83c0ed9e34e8330b9b1516b2ca323704ca1b24db
Victor Klijmeij <victorklijmeij@users.noreply.github.com>
2015-06-28 22:16:34 +0200

    space/tabs

--- spaceapi.py
+++ spaceapi.py
@@ -18,14 +18,14 @@ class Configuration(object):
 	"""
 	Configuration loader
 	"""
-    def __init__(self, *file_names):
-        parser = SafeConfigParser()
-        parser.optionxform = str 
-        found = parser.read(file_names)
-        if not found:
-            raise ValueError('No config file found!')
-        for name in section_names:
-            self.__dict__.update(parser.items(name)) 
+	def __init__(self, *file_names):
+		parser = SafeConfigParser()
+		parser.optionxform = str 
+		found = parser.read(file_names)
+		if not found:
+			raise ValueError('No config file found!')
+		for name in section_names:
+			self.__dict__.update(parser.items(name)) 
 			
 config = Configuration('space.cfg')
 config.state=False
